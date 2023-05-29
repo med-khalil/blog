722951d556f5b8f6e1e5bf17a7dbd7961906382f
Mohamed Khalil Ben Abdallah <medkhalil.benabdallah@expensya.com>
2023-05-30 00:16:34 +0100

    add some tests to the app

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from blog.models import Article
+from users.models import User
+from django.urls import reverse
+
+# test Article functionality : 
+class ArticleTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser')
+        self.article = Article.objects.create(
+            title='Test Article',
+            description='Test Description',
+            body='Test Body',
+            author=self.user,
+        )
+
+    def test_article_str(self):
+        self.assertEqual(str(self.article), 'Test Article')
+
+    def test_article_ordering(self):
+        articles = Article.objects.all()
+        self.assertEqual(list(articles), [self.article])
